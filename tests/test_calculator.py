f6d3bbeb5bc9241374734602f1271b68c512cfdb
AlvaroRiveraCarhuani <alvarorivera4513@gmail.com>
2025-06-05 15:15:38 -0400

    Agregacion de nueva funcion: multiplicacion

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicacion
 
 class CalculatorTest(unittest.TestCase):
    
@@ -9,3 +9,8 @@ class CalculatorTest(unittest.TestCase):
 
     def test_resta(self):
         self.assertEqual(resta(10, 5), 5)
+    
+    def test_multipliacion(self):
+        self.assertEqual(multiplicacion(5,2),10)
+    
+    
